d490367e78c406200c0636c95e9f329aecef8329
DMorrison <declan@morrison-okeefe.com>
2018-07-02 21:33:08 -0400

    declanmorrison added button function

--- assets/Javascript/main.js
+++ assets/Javascript/main.js
@@ -8,60 +8,72 @@ Array of all cards
 
 */
 
-var cards = [
-"2D",
-"3D",
-"4D",
-"5D",
-"6D",
-"7D",
-"8D",
-"9D",
-"10D",
-"JD",
-"QD",
-"KD",
-"AD",
+function cardRandomizer(){
+    cardHand = [];
+    while (cardHand.length < 5){
+        var value = Math.floor(Math.random() * 13) + 2;
+        var suit = Math.floor(Math.random() * 4);
 
-"2S",
-"3S",
-"4S",
-"5S",
-"6S",
-"7S",
-"8S",
-"9S",
-"10S",
-"JS",
-"QS",
-"KS",
-"AS",
+        switch (value){
+            case 14:
+                value = "A";
+                break;
 
-"2C",
-"3C",
-"4C",
-"5C",
-"6C",
-"7C",
-"8C",
-"9C",
-"10C",
-"JC",
-"QC",
-"KC",
-"AC",
+            case 13:
+                value = "K";
+                break;
 
-"2H",
-"3H",
-"4H",
-"5H",
-"6H",
-"7H",
-"8H",
-"9H",
-"10H",
-"JH",
-"QH",
-"KH",
-"AH",
-]
+            case 12:
+                value = "Q";
+                break;
+            
+            case 11:
+                value = "J";
+                break;
+
+            default:
+
+        };
+
+        switch (suit){
+            case 0:
+                suit = "S";
+                break;
+
+            case 1:
+                suit = "C";
+                break;
+
+            case 2:
+                suit = "D";
+                break;
+
+            case 3:
+                suit = "H";
+                break;
+
+            default:
+
+        };
+
+        var card = (value + suit);
+
+        cardHand.push(card);
+        console.log(cardHand);
+    };
+    
+
+    $.each(cardHand, function(index, val){
+
+        var cardNumber = (index + 1);
+        var cardSelector = cardNumber.toString();
+        console.log(cardSelector);
+        console.log("cardSelector is " + typeof cardSelector);
+
+        var cardHref = ("./assets/images/" + val + ".png");
+        console.log(cardHref);
+        console.log("cardHref is " + typeof cardHref);
+
+        $(".card-" + cardSelector).attr("src", cardHref);
+    });
+};
